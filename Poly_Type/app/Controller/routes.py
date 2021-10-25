97b874370b7fcebb61817c8ffd6c40e8bd479050
Brandon Cook <brandoncook7000@gmail.com>
2021-10-25 16:17:50 -0700

    Built createCode function and createChallenge route

--- Poly_Type/app/Controller/routes.py
+++ Poly_Type/app/Controller/routes.py
@@ -3,10 +3,14 @@ import sys
 from flask import Blueprint
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_required
+#from Poly_Type.app.Model.models import Challenge
+from app.Model.models import Challenge
 from app.Model.models import Host
 from app import db
 from config import Config
 from app.Controller.forms import CreateChallengeForm, RegistrationForm, TakeChallengeForm, LoginForm
+import random
+import string
 
 bp_routes = Blueprint('routes', __name__)
 bp_routes.template_folder = Config.TEMPLATE_FOLDER #'..\\View\\templates'
@@ -30,9 +34,37 @@ def index():
 
     return render_template('index.html', joinForm = joinForm, loginForm = loginForm, registrationForm = registrationForm)
 
+
+def createCode():
+    code = ''
+    for i in range(0, 5):
+        randChar = random.choice(string.ascii_uppercase)#Create a random capital letter
+        if((random.randint(0, 1) == 1) and (randChar != 'I') and (randChar != 'O')):#if the random int is 1 and not 'O' or 'I' add it
+            code += randChar
+        else:#add a random digit if random int is 0
+            code += random.choice(string.digits)
+
+    return code
+
 @bp_routes.route('/createchallenge', methods=['GET', 'POST'])
 def createChallenge():
     challengeForm = CreateChallengeForm()
+
+    #print(code)
+    if challengeForm.validate_on_submit():
+        code = createCode()#Creates a random code
+
+        while((db.session.query.filter_by(joincode=code).first()) != None):#If random code is already used keep looping until it finds one that is not used
+            code = createCode()
+        
+        challenge = Challenge(joincode = code, open=False, )#Create a new challenge with the random code
+        for challenge in challengeForm.challenge.data:
+            if challenge.prompt is not None:
+                challenge.challenges.append(challenge)
+        db.session.add(challenge)
+        db.session.commit()
+        flash('Challenge created!')
+        return redirect(url_for('routes.index'))
     return render_template('createChallenge.html', challengeForm = challengeForm)
     
 
