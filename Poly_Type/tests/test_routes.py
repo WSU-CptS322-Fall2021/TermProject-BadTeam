8c839d55627e34beeee4e11c8357b42d5047b6aa
Brandon Cook <brandoncook7000@gmail.com>
2021-11-24 14:29:08 -0800

    Fixed Unit test explorer not detecting by adding a __init__.py file.

--- Poly_Type/tests/test_routes.py
+++ Poly_Type/tests/test_routes.py
@@ -1,7 +1,7 @@
 import unittest
 import sys
 #from app.Controller.hostRoutes import createCode
-from app.Controller.hostRoutes import createCode
+from app.Controller.hostRoutes import createCode, view_challenges
 class TestRoutes(unittest.TestCase):
 
     def test_createcode(self):
@@ -9,3 +9,8 @@ class TestRoutes(unittest.TestCase):
         self.assertTrue(len(code) == 6)
         for i in range(0, 5):
             self.assertTrue(code[i] in 'ABCDEFGHJKLMNPQRSTUVWXYZ1234567890')
+    def test_view_challenges(self):
+        print("test")
+        #tester = view_challenges(self)x
+        #response = tester.get('/view_challenges')
+        #self.assertTrue(response.status_code, 200)
