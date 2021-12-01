8554c3813d798a7818a0bf01921a728875fce231
Brandon Cook <brandoncook7000@gmail.com>
2021-11-30 16:30:49 -0800

    Create foundation for testing with pytest.

--- Poly_Type/tests/test_routes.py
+++ Poly_Type/tests/test_routes.py
@@ -1,9 +1,60 @@
+import os
 import unittest
+import pytest
 import sys
+from config import Config
+from app import create_app, db
+from app.Model.models import Host
 #from app.Controller.hostRoutes import createCode
 from app.Controller.hostRoutes import createCode, view_challenges
-class TestRoutes(unittest.TestCase):
 
+
+class TestConfig(Config):
+    TESTING = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+@pytest.fixture(scope='module')
+def test_client():
+    flask_app = create_app(config_class=TestConfig)
+
+    db.init_app(flask_app)
+    testing_client = flask_app.test_client()
+ 
+    # Establish an application context before running the tests.
+    ctx = flask_app.app_context()
+    ctx.push()
+ 
+    yield  testing_client 
+    # this is where the testing happens!
+ 
+    ctx.pop()
+
+# Function to create a user for testing
+def new_user(uname, passwd):
+    user = Host(username=uname)
+    user.set_password(passwd)
+    return user
+
+# Function to intialize the database
+@pytest.fixture
+def init_database():
+    #Create the database
+    db.create_all()
+
+    #Create the test user
+    user = new_user('test', '1234')
+
+    #Add the user to the database
+    db.session.add(user)
+    db.session.commit()
+
+    yield
+
+    db.drop_all()
+
+'''
+class TestRoutes(unittest.TestCase):
     def test_createcode(self):
         code = createCode()
         self.assertTrue(len(code) == 6)
@@ -14,3 +65,4 @@ class TestRoutes(unittest.TestCase):
         #tester = view_challenges(self)x
         #response = tester.get('/view_challenges')
         #self.assertTrue(response.status_code, 200)
+'''
