6d7503268c84e0aa0b503537bc985347f7be99f9
Brandon Cook <brandoncook7000@gmail.com>
2021-12-06 14:01:21 -0800

    Fixed a couple bugs in the tests.

--- Poly_Type/tests/test_routes.py
+++ Poly_Type/tests/test_routes.py
@@ -1,13 +1,18 @@
 import os
+from typing import Text
 import unittest
 import pytest
 import sys
+
+from wtforms.fields.simple import TextAreaField
+from app.Controller.forms import PromptForm
 from config import Config
 from app import create_app, db
 from app.Model.models import Host
 from app.Controller.hostRoutes import *
 #from app.Controller.hostRoutes import createCode, view_challenges, create_challenge
 from app.Controller.challengerRoutes import *
+from wtforms.fields.core import FieldList, FormField
 
 
 class TestConfig(Config):
@@ -81,7 +86,7 @@ def test_index(test_client):
 def test_index_register(test_client, init_database):
 
     #IMPORTANT - If you want to specify the form make sure to set the submit value to the name of the button
-    response = test_client.post('/index', data=dict(reg_username='test_register',reg_password='12345', reg_password2='12345', submit='Register'), follow_redirects=True)
+    response = test_client.post('/index', data=dict(reg_username='test_register',reg_password='12345', reg_password2='12345', register='Register'), follow_redirects=True)
     assert response.status_code == 200
 
     s = db.session.query(Host).filter(Host.username=='test_register').first()
@@ -92,7 +97,7 @@ def test_index_register(test_client, init_database):
     assert b'Create a New Challenge' in response.data
 def test_login_logout(test_client, init_database):
     #First login
-    response = test_client.post('/index', data=dict(username='test_login', password='1234', submit='Login'), follow_redirects=True)
+    response = test_client.post('/index', data=dict(username='test_login', password='1234', login='Login'), follow_redirects=True)
     assert response.status_code == 200
 
     s = db.session.query(Host).filter(Host.username=='test_login').first()
@@ -108,17 +113,24 @@ def test_login_logout(test_client, init_database):
 
 def test_create_challenge(test_client, init_database):
     #First login
-    response = test_client.post('/index', data=dict(username='test_login', password='1234', submit='Login'), follow_redirects=True)
+    response = test_client.post('/index', data=dict(username='test_login', password='1234', login='Login'), follow_redirects=True)
     assert response.status_code == 200
 
     s = db.session.query(Host).filter(Host.username=='test_login').first()
     assert s.username == 'test_login'
     assert s.check_password('1234')
     #Then create a challenge
-    test_prompt = [Prompt(text="The brown fox jumped over the white fence"), Prompt(text="Please take your dog, Cali, out for a walk – he really needs some exercise!"), Prompt(text="When do you think they will get back from their adventure in Cairo, Egypt?")]
     
-    response = test_client.post('/create_challenge', data=dict(title='test_challenge_create', prompts=test_prompt), follow_redirects=True)
-    assert response.status_code == 200
+    #test_prompt = [Prompt(text="The brown fox jumped over the white fence"), Prompt(text="Please take your dog, Cali, out for a walk – he really needs some exercise!"), Prompt(text="When do you think they will get back from their adventure in Cairo, Egypt?")]
+    #test_prompt = FieldList(FormField(PromptForm(data=dict(prompt=TextAreaField("The brown fox jumped over the white fence")))), FormField(PromptForm(data=dict(prompt=TextAreaField("Please take your dog, Cali, out for a walk – he really needs some exercise!")))), FormField(PromptForm(data=dict(prompt=TextAreaField("When do you think they will get back from their adventure in Cairo, Egypt?")))))
+    prompts = [{'prompt': 'The brown fox jumped over the white fence'}, {'prompt': 'Please take your dog, Cali, out for a walk – he really needs some exercise!'}, {'prompt': 'When do you think they will get back from their adventure in Cairo, Egypt?'}]
+    #prompts = data=dict(prompt='The brown fox jumped over the white fence')
+    #prompts = PromptForm(data=dict(prompt='The brown fox jumped over the white fence'))
+    #prompts = FieldList(FormField(prompt="The brown fox jumped over the white fence"), FormField(prompt="Please take your dog, Cali, out for a walk – he really needs some exercise!"), FormField(prompt="When do you think they will get back from their adventure in Cairo, Egypt?"))
+    #prompts = FieldList(FormField(PromptForm(prompt="The brown fox jumped over the white fence")), FormField(PromptForm(prompt="Please take your dog, Cali, out for a walk – he really needs some exercise!")), FormField(PromptForm(prompt="When do you think they will get back from their adventure in Cairo, Egypt?")))
+    #prompt1 = ["The brown fox jumped over the white fence", "Please take your dog, Cali, out for a walk – he really needs some exercise!", "When do you think they will get back from their adventure in Cairo, Egypt?", "When do you think they will get back from their adventure in Cairo, Egypt?"]
+    response2 = test_client.post('/create_challenge', data=dict(title='test_challenge_create', prompts=prompts, create_challenge='Post'), follow_redirects=True)
+    assert response2.status_code == 200
     c = db.session.query(Challenge).filter(Challenge.title=='test_challenge_create').first()
     assert c.host_id == s.id
 
@@ -131,32 +143,13 @@ def test_take_challenge(test_client, init_database):
     assert response.status_code == 200
     assert tc.open == True
 
-'''
-def test_close_open_challenge(test_client, init_database):
-    tc = db.session.query(Challenge).filter(Challenge.title=='test_challenge').first() #Get the challenge
-
-    response = test_client.post('/index', data=dict(nickname='test', joincode=tc.joincode, submit='Join'), follow_redirects=True)
-    #Check you got the challenge page
-    assert response.status_code == 200
-    assert tc.open == True
-
-    #Get the challengeid
-    c_id = tc.id
-
-    #Test closing the challenge
-    response = test_client.post('/close_challenge/<challengeid>', challengeid=c_id, data=dict(joincode=tc.joincode), follow_redirects=True)
-    #Check if you were redirected to the challenge page and if the challenge is open
-    assert response.status_code == 200
-    assert tc.open == False
-'''
-
 def test_edit_host(test_client, init_database):
-    response = test_client.post('/index', data=dict(username='test_login', password='1234', submit='Login'), follow_redirects=True)
+    response = test_client.post('/index', data=dict(username='test_login', password='1234', login='Login'), follow_redirects=True)
     
     s = db.session.query(Host).filter(Host.username=='test_login').first()
     assert s.username == 'test_login'
     #Change username
-    response2 = test_client.post('/update_info', data=dict(reg_username='test_login_new', reg_password='1234', reg_password2='1234'), follow_redirects=True)
+    response2 = test_client.post('/edit_host', data=dict(reg_username='test_login_new', reg_password='1234', reg_password2='1234'), follow_redirects=True)
     assert response2.status_code == 200
 
     #Then check if the username was changed
@@ -176,16 +169,8 @@ def test_result(test_client, init_database):
     #Then reroute to the result page
     response2 = test_client.post('/view_challenges', data=dict(submit='challengerresult'), follow_redirects=True)
     assert response2.status_code == 200
-'''
-class TestRoutes(unittest.TestCase):
-    def test_createcode(self):
-        code = createCode()
-        self.assertTrue(len(code) == 6)
-        for i in range(0, 5):
-            self.assertTrue(code[i] in 'ABCDEFGHJKLMNPQRSTUVWXYZ1234567890')
-    def test_view_challenges(self):
-        print("test")
-        #tester = view_challenges(self)x
-        #response = tester.get('/view_challenges')
-        #self.assertTrue(response.status_code,xzx 200)
-'''
+def test_createcode(test_client):
+    code = createCode()
+    assert len(code) == 6
+    for i in range(0, 5):
+        assert code[i] in 'ABCDEFGHJKLMNPQRSTUVWXYZ1234567890'
